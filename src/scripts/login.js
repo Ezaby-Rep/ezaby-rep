1bfa9a580652fe5ccd64ebca7799d515f9946bee
Islam Sayed <islam.sayed8@gmail.com>
2023-05-14 21:11:59 +0300

    commit

---
+++ src/scripts/login.js
@@ -0,0 +1,11 @@
+const passInput = document.querySelector('input[type=password]');
+const
+passInput.addEventListener('input', (e) => {
+  console.log(e.target.value);
+  if (e.target.value === 'islam2023') {
+    window.location.replace('/tasks/فرش%20الفروع%20الجديدة')
+  } else {
+
+  }
+})
+
---
+++ src/scripts/typewriter.js
@@ -0,0 +1,26 @@
+let textLength = 0;
+let text =`هذا العمل هو نتاج مجهود جماعى شارك فيه كل أفراد القسم.. 
+
+الهدف الأساسى منه هو توثيق لجميع المهام التى يضطلع بها القسم و جعلها مرجعا لأى عضو جديد..`
+
+const btn = document.querySelector('.login-btn')
+
+function type() {
+	let textChar = text.charAt(textLength++);
+	let paragraph = document.getElementById('typed');
+	let charElement = document.createTextNode(textChar);
+	paragraph.appendChild(charElement);
+	if (textLength < text.length + 1) {
+		setTimeout(() => type(), 50);
+	} else {
+		text = '';
+
+    setTimeout(() => {
+      btn.style.visibility = 'visible';
+    }, 500);
+	}
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+	type();
+});
