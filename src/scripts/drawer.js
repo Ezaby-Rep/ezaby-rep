4e4a5d496d633d94a89caceb42c6c7dd063a2cdf
Islam Sayed <islam.sayed8@gmail.com>
2023-05-14 15:41:54 +0300

    responsive style

---
+++ src/scripts/drawer.js
@@ -0,0 +1,15 @@
+const burger = document.querySelector('.burger')
+const drawer = document.querySelector('aside')
+const asideText = document.querySelector('aside ul')
+burger.addEventListener('click', () => {
+  if (drawer.classList.contains('open-drawer')) {
+    drawer.classList.add('close-drawer')
+    drawer.classList.remove('open-drawer')
+    asideText.classList.add('remove-txt')
+  } else {
+    drawer.classList.add('open-drawer')
+    drawer.classList.remove('close-drawer')
+    asideText.classList.remove('remove-txt')
+
+  }
+})
