c49bcfe0af4f6a32b6e072b7ef3f460f667371e7
Islam Sayed <islam.sayed8@gmail.com>
2023-05-16 19:34:32 +0300

    fix bug

--- src/scripts/drawer.js
+++ src/scripts/drawer.js
@@ -4,9 +4,10 @@ const asideText = document.querySelector('aside ul')
 
 burger.addEventListener('click', () => {
   if (drawer.classList.contains('open-drawer')) {
+    asideText.classList.add('remove-txt')
+    
     drawer.classList.add('close-drawer')
     drawer.classList.remove('open-drawer')
-    asideText.classList.add('remove-txt')
   } else {
     drawer.classList.add('open-drawer')
     drawer.classList.remove('close-drawer')
