61fd83af6221ba3f716749f4c0c9368fbf051355
islamCodehood <islam.sayed8@gmail.com>
2023-06-30 19:02:17 +0300

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,17 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from "@astrojs/tailwind";
+
+import sanity from "astro-sanity";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [
+    tailwind(), 
+    sanity({
+      projectId: 'x44fdslf',
+      dataset: 'production',
+      apiVersion: '2021-03-25',
+      useCdn: true,
+    })
+  ]
 });
---
+++ sanity-first/sanity.cli.ts
@@ -0,0 +1,8 @@
+import {defineCliConfig} from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: 'x44fdslf',
+    dataset: 'production'
+  }
+})
